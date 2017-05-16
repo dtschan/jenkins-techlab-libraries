fb262b61841714f729cdb1214ddf999ae18fb8be
Daniel Tschan <tschan+github@puzzle.ch>
2017-05-16 16:21:15 +0200

    Update notifyPuzzleChat.groovy

--- vars/notifyPuzzleChat.groovy
+++ vars/notifyPuzzleChat.groovy
@@ -1,16 +1,10 @@
-def call(String channel = null) {
-    echo currentBuild.result
-    try {
+def call(String channel) {
         String result = currentBuild.result?.toLowerCase() ?: 'success'
-    } catch(Exception e) {
-        echo e.toString()
+        node {
+            rocketSend(
+                channel: channel,
+                avatar: "https://chat.puzzle.ch/emoji-custom/${result}.png",
+                message: "Build ${result} - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)",
+                rawMessage: true)
+        }
     }
-    echo result
-    node {
-        rocketSend(
-            channel: channel,
-            avatar: "https://chat.puzzle.ch/emoji-custom/${result}.png",
-            message: "Build success - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)",
-            rawMessage: true)
-    }
-}
